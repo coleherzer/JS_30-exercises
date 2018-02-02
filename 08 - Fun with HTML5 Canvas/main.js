4e5093c1be9147cd49c7eb1a59dc9288abaac4c4
Cole Herzer <cole.herzer@gmail.com>
2018-02-02 10:53:44 -0800

    fixed js file, and added initial JS for canvas

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -1,2 +1,24 @@
 console.log('hi, js is connected')
 
+const canvas = document.querySelector('#draw')
+
+const ctx = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+ctx.strokeStyle = '#BADA55'
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
+
+let isDrawing = false
+
+let lastX = 0
+let lastY = 0
+
+function draw (evt) {
+    console.log(evt)
+}
+
+canvas.addEventListener('mousemove', draw)
+
