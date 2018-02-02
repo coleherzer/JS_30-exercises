bb0b9e3c26894f9c0a8ce9fa63a3cafdaed26352
Cole Herzer <cole.herzer@gmail.com>
2018-02-02 11:00:57 -0800

    added event listeners and began js for drawing

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -17,8 +17,28 @@ let lastX = 0
 let lastY = 0
 
 function draw (evt) {
-    console.log(evt)
+    if (!isDrawing) {
+        return 
+        // will stop the function when they are not clicking down
+    } else {
+        console.log(evt)
+        ctx.beginPath()
+        ctx.moveTo(lastX, lastY)
+        ctx.lineTo(evt.offsetX, evt.offsetY)
+        ctx.stroke()
+    }
 }
 
 canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true
+})
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false
+})
+canvas.addEventListener('mouseout', () => {
+    isDrawing = false
+})
+
+
 
