53af2acac7b757c078eec1f2d567b6d9e9756458
Cole Herzer <cole.herzer@gmail.com>
2018-02-13 11:14:05 -0800

    wrote examples for interpolation and styled console logs

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -8,13 +8,22 @@ function makeGreen() {
   p.style.fontSize = '50px';
 }
 
+// in the inspector, can right click on the element and selecting break on, and then selecting attribute
+// modification to bring up a debugger to see exactly what's happening
+
 // Regular
+console.log('hello')
 
 // Interpolated
+// after the comma is what will be input in the %s position
+console.log('hello, this is a %s string', 'YOOOOOO')
+// ES6 back ticks used more often now
 
 // Styled
+console.log('%c I am some large text', 'font-size: 50px; background: blue')
 
 // warning!
+
 
 // Error :|
 
