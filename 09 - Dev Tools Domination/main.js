4385a2bac263b102107549b5cec2d14d9a164bb3
Cole Herzer <cole.herzer@gmail.com>
2018-02-13 11:38:22 -0800

    finished up exercise examples

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -36,14 +36,50 @@ console.info('Leopards can carry 2x their body weight into a tree')
 // didnt work?
 
 // Testing
-console.assert(1 === 2, 'Incorrect')
+const p = document.querySelector('p')
+
+console.assert(p.classList.contains('ouch'), 'Incorrect class')
+// console.assert(1 === 2, 'Incorrect')
 
 // clearing
+console.clear()
+//clears the console
 
 // Viewing DOM Elements
+console.log(p)
+// gives you the element
+
+console.dir(p)
+// gives you the element as a dropdown so you can see all the attributes and all that
 
 // Grouping together
+dogs.forEach((dog) => {
+    console.group(`${dog.name}`)
+    // can use groupCollapsed above so they display as a drop down on one line to start
+    console.log(`this is ${dog.name}`)
+    console.log(`this dog is ${dog.age} years old`)
+    console.log(`this dog is ${dog.age * 7} years old in dog years`)
+    console.groupEnd(`${dog.name}`)
+})
+// groups each loop of console logs together to see easily
 
 // counting
+// counts however many times you use a specific word, var, object, DOM node, etc
+console.count('Cole')
+console.count(`${dogs}`)
 
 // timing
+// to see how long something takes
+
+console.time('fetching data...')
+
+fetch('https://api.github.com/users/coleherzer')
+    .then((data) => {
+        data.json()
+    }).then((data) => {
+        console.timeEnd('fetching data...')
+        console.log(data)
+    })
+
+
+console.table(dogs)
