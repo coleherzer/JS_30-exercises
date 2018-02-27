74b57b54644a779091ca1367cface36e6571a021
Cole Herzer <cole.herzer@gmail.com>
2018-02-27 14:08:21 -0800

    finished checking logic and exercise

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -8,6 +8,25 @@ let lastChecked;
 
 function handleCheck (evt) {
     console.log(evt)
+    // need to check if they were holding down shift key
+    // AND check if they are checking it (not unchecking the box)
+    let inBetween = false
+    if(evt.shiftKey && this.checked) {
+        // loop over every single check box
+        checkBoxes.forEach((box) => {
+            console.log(box)
+            if(box === this || box === lastChecked) {
+                inBetween = !inBetween
+            }
+            if(inBetween) {
+                box.checked = true
+            }
+        })
+
+    }
+
+
+    lastChecked = this
 }
 
 checkBoxes.forEach((box) => {
