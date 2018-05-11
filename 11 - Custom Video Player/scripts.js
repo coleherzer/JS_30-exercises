e4cd669812d39f67696960218910006c1d1185bd
Cole Herzer <cole.herzer@gmail.com>
2018-05-11 08:23:22 -0700

    added initial functions and event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,10 +5,29 @@ const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 
 const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelectorAll([data-skip])
+const skipButtons = player.querySelectorAll('[data-skip]')
 const ranges = player.querySelectorAll('.player__slider')
 
 
 // Build our functions
+function togglePlay() {
+    //video.paused is a built in property of the video element
+    if(video.paused) {
+        video.play()
+    } else {
+        video.pause()
+    }
+}
+
+function updateButton() {
+    console.log('updated the button')
+}
 
 // Hook up event listeners
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+
+toggle.addEventListener('click', togglePlay)
+
+
+
